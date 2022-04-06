546e7ead1d1425a43dbe1a839f04c251761faa50
magurman <mattgurman@gmail.com>
2022-04-06 14:34:59 -0700

    divider styling

--- frontend/src/components/Divider.tsx
+++ frontend/src/components/Divider.tsx
@@ -1,14 +1,6 @@
 import styled from "styled-components";
 
 export const Divider = styled.div`
-    border-top: 2px solid darkgrey;
+    border-top: 4px dashed darkgrey;
     grid-column: 1 / 1; /* this code makes the row stretch to entire width of the grid */ 
 `;
-
-// const Divider = () => {
-//     return (
-//         <div>
-
-//         </div>
-//     )
-// }
